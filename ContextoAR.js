808dab2ec15eed96db849eeaca15f3c998b40cdb
danielLoaiza <cohl.daniel@gmail.com>
2020-11-26 21:04:43 -0300

    Cambiar Direccion a calibracion de cam

--- ContextoAR.js
+++ ContextoAR.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js';
-THREEx.ArToolkitContext.baseURL = '../../clase 10/ARImagen/';
+THREEx.ArToolkitContext.baseURL = '../testImageCampusAR/';
 export class ContextoAR{
     constructor(mundo){
         this.mundo = mundo;
