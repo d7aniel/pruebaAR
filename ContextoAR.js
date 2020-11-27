76cf5dbfd976a492db6ddae5ee3db630cf100af6
danielLoaiza <cohl.daniel@gmail.com>
2020-11-26 21:00:32 -0300

    Agregar Pagina

---
+++ ContextoAR.js
@@ -0,0 +1,71 @@
+import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js';
+THREEx.ArToolkitContext.baseURL = '../../clase 10/ARImagen/';
+export class ContextoAR{
+    constructor(mundo){
+        this.mundo = mundo;
+        //------- AR Toolkit Source, es decir lo que la camara ve
+        this.arToolkitSource = new THREEx.ArToolkitSource({
+            sourceType : 'webcam'
+        });
+        //--- El source se inicializa en un proceso por separado
+        var tmpresize = this.onResize;
+        var tmparToolkitSource = this.arToolkitSource;
+        var tmparToolkitContext = this.arToolkitContext;
+        var tmprenderizador = mundo.renderizador;
+        this.arToolkitSource.init(function onReady(){
+            // necesitamos una funcion para cambiar el tamaño de la ventana cuando se inicia la camara
+            setTimeout(function() {
+                tmpresize(tmparToolkitSource,tmparToolkitContext,tmprenderizador);
+            }, 2000);
+        });
+        window.addEventListener('resize', function(){
+            tmpresize(tmparToolkitSource,tmparToolkitContext,tmprenderizador);
+        });
+
+        // agregamos este evento para cuando los marcadores nft acabaron de cargar nos avisa
+       window.addEventListener('arjs-nft-loaded', function(ev){
+         console.log(ev);
+       })
+
+        //-------- AR Toolkit Context
+        this.arToolkitContext = new THREEx.ArToolkitContext({
+            cameraParametersUrl: THREEx.ArToolkitContext.baseURL + 'camera_para.dat',
+            detectionMode: 'mono'
+        });
+        //-- inicializar
+        var tmparToolkitContext = this.arToolkitContext;
+        this.arToolkitContext.init(function onCompleted(){
+            // copy projection matrix to camera
+            mundo.camara.projectionMatrix.copy( tmparToolkitContext.getProjectionMatrix() );
+        });
+
+
+    }
+
+    crearDescriptor(archivo, nombre){
+        var raizDelMarcador = new THREE.Group;
+        raizDelMarcador.name = nombre;
+        this.mundo.escena.add(raizDelMarcador)
+        this.markerControls = new THREEx.ArMarkerControls(this.arToolkitContext, raizDelMarcador, {
+            type : 'nft',//natural feature tracking
+            descriptorsUrl  : THREEx.ArToolkitContext.baseURL + archivo,
+            changeMatrixMode: 'modelViewMatrix'
+        });
+        return raizDelMarcador;
+    }
+
+    onResize(arToolkitSource,arToolkitContext,renderizador){
+        if(arToolkitSource.domElement!== null && arToolkitContext !== undefined){
+            arToolkitSource.onResizeElement();
+            arToolkitSource.copyElementSizeTo(renderizador.domElement);
+            if( arToolkitContext.arController !== null ){
+                arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
+            }
+        }
+    }
+
+    actualizar(){
+        if( this.arToolkitSource.ready === false )	return;
+    	this.arToolkitContext.update( this.arToolkitSource.domElement );
+    }
+}
---
+++ script.js
@@ -0,0 +1,118 @@
+import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js';
+import {MundoAR} from './MundoAR.js';
+import {ContextoAR} from './ContextoAR.js';
+import {cargarModelo} from './CargarModelo.js';
+//import { Reflector } from 'https://unpkg.com/three@0.122.0/examples/jsm/objects/Reflector.js';
+
+var mundo;
+var modelos = [];
+var contextoAR;
+
+function iniciar(){
+    mundo = new MundoAR();
+    contextoAR = new ContextoAR(mundo);
+    //mundo.iluminar();
+    mundo.iluminarConFoto('./hdr/fondo.png',false);
+
+
+
+
+    /*for(var i=0;i<5;i++){
+        if(i!=2){
+            var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+            var material = new THREE.MeshStandardMaterial( {color: 0xfffff*Math.random()} );
+            modelos[i] = new THREE.Mesh( geometry, material );
+            modelos[i].position.set((i*2)-4,0,0);
+            mundo.escena.add( modelos[i] );
+        }else{
+            modelos[i] = new THREE.Object3D();
+            cargarModelo('./modelo/espada.glb',mundo,modelos[i]);
+        }
+    }*/
+
+
+
+    var descriptor = contextoAR.crearDescriptor('descriptor/cara');
+
+    /*var geometriaCubo = new THREE.BoxBufferGeometry( 75, 75, 75 );
+    var material = new THREE.MeshStandardMaterial( {color: 0xffffff*Math.random(0), roughness: 1, metalness: 0} );
+    modelos[0] = new THREE.Mesh( geometriaCubo, material );
+    modelos[0].position.z-= 80;
+    modelos[0].position.x+= 60;
+    modelos[0].position.y+= 37.5;
+    descriptor.add( modelos[0] );*/
+
+    modelos[0] = new THREE.Object3D();
+    cargarModelo('./modelo/modeloLD.glb',descriptor,modelos[0]);
+    modelos[0].scale.x=35
+    modelos[0].scale.y=35
+    modelos[0].scale.z=35
+    modelos[0].position.z-= 80;
+    modelos[0].position.x+= 60;
+    modelos[0].position.y+= 17.5;
+/*
+    var vidrio = new THREE.MeshPhysicalMaterial( {
+        color: 0xffffff,
+        metalness: 0,
+        roughness: 0,
+        alphaTest: 0.5,
+        depthWrite: false,
+        envMapIntensity:1,
+        transmission: 0.9, // use material.transmission for glass materials
+        opacity: 1, // set material.opacity to 1 when material.transmission is non-zero
+        transparent: true
+    } );
+
+    modelos[1] = new THREE.Mesh( geometriaCubo, vidrio );
+    modelos[1].position.x+=1.2;
+    mundo.escena.add( modelos[1] );
+
+    var material = new THREE.MeshStandardMaterial( {color: 0xffffff, roughness: 0, metalness: 1} );
+    modelos[3] = new THREE.Mesh( geometriaCubo, material );
+    modelos[3].position.x-=1.2;
+    mundo.escena.add( modelos[3] );
+
+    var piso = new THREE.PlaneBufferGeometry( 5,5 );
+	const espejo = new Reflector( piso, {
+		clipBias: 0.003,
+		textureWidth: window.innerWidth * window.devicePixelRatio,
+		textureHeight: window.innerHeight * window.devicePixelRatio,
+		color: 0x889999
+	} );
+    espejo.position.y-=0.6;
+    espejo.rotateX(-Math.PI/2);
+    mundo.escena.add( espejo );
+
+    //var piso = new THREE.PlaneBufferGeometry( 5,5 );
+	const espejo2 = new Reflector( piso, {
+		clipBias: 0.003,
+		textureWidth: window.innerWidth * window.devicePixelRatio,
+		textureHeight: window.innerHeight * window.devicePixelRatio,
+		color: 0x889999
+	} );
+    espejo2.position.y-=0.65;
+    espejo2.rotateX(Math.PI/2);
+    mundo.escena.add( espejo2 );
+
+
+    var geometriaEsfera = new THREE.SphereBufferGeometry( 0.5 );
+    var material = new THREE.MeshStandardMaterial( {color: 0xffffff*Math.random(0), roughness: 1, metalness: 0} );
+
+    modelos[4] = new THREE.Mesh( geometriaEsfera, material );
+    modelos[4].position.y-=1.5;
+    mundo.escena.add( modelos[4] );*/
+}
+
+function animar(){
+    requestAnimationFrame(animar);
+    /*for(var i=0;i<1;i++){
+        modelos[i].rotation.y+=0.01;
+        modelos[i].rotation.x+=0.001;
+        modelos[i].rotation.z+=0.005;
+    }*/
+    contextoAR.actualizar();
+    mundo.dibujar();
+}
+
+iniciar();
+animar();
