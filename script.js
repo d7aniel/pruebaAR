c186f963280a7a2b05c8d54888c68ec66ce441ea
danielLoaiza <cohl.daniel@gmail.com>
2021-12-22 14:21:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -108,8 +108,8 @@ function iniciar(){
 
 function animar(){
     requestAnimationFrame(animar);
-	 modelos[0].position.lerp(posicion[0].position,0.2);
-	 modelos[0].rotation.lerp(posicion[0].rotation,0.2);
+	 modelos[0].position.lerp(posiciones[0].position,0.2);
+	 modelos[0].rotation.lerp(posiciones[0].rotation,0.2);
     /*for(var i=0;i<1;i++){
         modelos[i].rotation.y+=0.01;
         modelos[i].rotation.x+=0.001;
